f43cab1eeb8b66347d310de09b5ed597493ee27f
dbond <debon1426@gmail.com>
2020-02-17 14:23:55 -0500

    BasicStringUtils completed.

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,20 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        String [] strArray = str.split("");
+        String result ="";
+
+        for ( int i=0; i<=strArray.length-1;i++){
+            if ((strArray[0].equals(strArray[i]))){
+                result = strArray[0].toUpperCase();
+            } else
+                result+=strArray[i];
+
+
+
+        }
+
+        return result;
     }
 
     /**
@@ -17,7 +30,14 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        String [] resultArray = str.split("");
+        String result="";
+
+        for (int i=resultArray.length-1; i>=0; i--){
+            result+=resultArray[i];
+        }
+
+        return result;
     }
 
     /**
@@ -25,7 +45,11 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+        String res =reverse(str);
+        String result= camelCase(res);
+
+
+        return result;
     }
 
 
@@ -34,7 +58,16 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+       String [] resultRemove = str.split("");
+       String result ="";
+       for ( int i=0; i<resultRemove.length; i++){
+           if (resultRemove[0]!= resultRemove[i] ){
+               if ( resultRemove[resultRemove.length-1]!= resultRemove[i])
+               result+=resultRemove[i];
+           }
+       }
+
+        return result;
     }
 
     /**
@@ -42,6 +75,20 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+  String [] strArray = str.split("");
+  String res ="";
+
+  for ( int i=0; i<=strArray.length-1;i++) {
+
+     if(Character.isUpperCase(str.charAt(i))){
+        res+= strArray[i].toLowerCase();
+     }  else   if(Character.isLowerCase(str.charAt(i))){
+          res+= strArray[i].toUpperCase();
+     }
+     else  res+= strArray[i];
+
+  }
+
+    return res;
     }
 }
