3943e4b4ae10a4bbb95d85fc6f4d200d45ff9da5
dbond <debon1426@gmail.com>
2020-02-17 11:45:55 -0500

    Cat  completed.

--- src/main/java/com/zipcodewilmington/assessment1/part3/Cat.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/Cat.java
@@ -4,11 +4,18 @@ package com.zipcodewilmington.assessment1.part3;
  * Created by leon on 2/16/18.
  */
 public class Cat extends Pet {
+    String name ="Cat name";
+    Integer age=0;
+
+
     /**
      * @param name name of this Cat
      * @param age age of this Cat
      */
     public Cat(String name, Integer age) {
+       this.name=name;
+      this.age=age;
+
 
     }
 
@@ -16,12 +23,14 @@ public class Cat extends Pet {
      * @param age age of this Cat
      */
     public Cat(Integer age) {
+        this.age=age;
     }
 
     /**
      * @param name name of this Cat
      */
     public Cat(String name) {
+        this.name=name;
 
     }
 
@@ -37,7 +46,26 @@ public class Cat extends Pet {
     /**
      * @return meow as a string
      */
+
+    // getters and setters for cat
     public String speak() {
-        return null;
+        return "Meow";
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
 }
